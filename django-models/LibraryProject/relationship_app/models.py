ea015d3b6ecdab0e6f6a95528b565f0ebcce3b47
Neyo <naledihloni14@gmail.com>
2025-11-14 11:33:08 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -40,12 +40,14 @@ user = User.objects.get()
 class UserProfile(models.Model): 
     "Admin", "Member"
 
+
 class User(AbstractUser):
     role = models.CharField(max_length=20, choices=[
         ('Admin', 'Admin'),
         ('Manager', 'Manager'),
         ('User', 'User')
     ])
+
 
 
 
