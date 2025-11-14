7a318ff310a1eec4491a3431de879441eddab2d8
Neyo <naledihloni14@gmail.com>
2025-11-14 11:50:50 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,6 +32,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 UserCreationForm()", "relationship_app/register.html
 
+@user_passes_test", 
+"relationship_app/member_view.html", 
+"relationship_app/librarian_view.html", "relationship_app/admin_view.html"
+
+
 
 
 
