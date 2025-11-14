82516eeb03540093063e0ab5b713e6ad8d55dc9f
Neyo <naledihloni14@gmail.com>
2025-11-14 11:24:58 +0200

    Update admin.py

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,7 +1,8 @@
 from .admin_site import AdminOnlySite
 from django.contrib import admin
-from .models import Book     # Example model
+from .models import Book     
 
 admin_site = AdminOnlySite(name="admin_only")
 
 admin_site.register(Book)
+
