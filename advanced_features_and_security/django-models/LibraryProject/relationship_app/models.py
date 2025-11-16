4ead691d3978654b9a22ae83e58ac22d6b4775dc
Neyo <naledihloni14@gmail.com>
2025-11-16 17:43:44 +0200

    Update models.py

--- advanced_features_and_security/django-models/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/django-models/LibraryProject/relationship_app/models.py
@@ -53,6 +53,24 @@ class User(AbstractUser):
 
 
 
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Birth(models.Model):
+    date_of_birth = models.DateField()
+
+
+
+
+
 
 
 
