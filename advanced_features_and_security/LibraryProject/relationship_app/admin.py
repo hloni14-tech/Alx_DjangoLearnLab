88f60150c001a28758509b66d6e23aafdbac2d60
Neyo <naledihloni14@gmail.com>
2025-11-16 18:06:02 +0200

    Delete advanced_features_and_security/LibraryProject directory

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++
@@ -1,8 +0,0 @@
-from .admin_site import AdminOnlySite
-from django.contrib import admin
-from .models import Book     
-
-admin_site = AdminOnlySite(name="admin_only")
-
-admin_site.register(Book)
-
