f3f8a22473ec1a4a327116ac45985965d2e09055
Neyo <naledihloni14@gmail.com>
2025-11-22 21:34:21 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,13 +2,16 @@ from rest_framework import generics
 from .models import Book, MyModel
 from .serializers import BookSerializer, MyModelSerializer
 from rest_framework import .viewsets.ModelViewSet
+from rst_framework.permissions import obtain_auth_token
 
 class BookListCreateAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookViewSetModelViewSet(generics.ModelViewSet):
-    
+
+
+
 
 
 
