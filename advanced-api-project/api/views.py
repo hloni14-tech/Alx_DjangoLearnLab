21d25aec9549ec0f623b3acb7aafe61535999505
Neyo <naledihloni14@gmail.com>
2025-11-30 18:58:44 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAuthenticated
 # Create your views here.
 ListView 
 DetailView 
 CreateView 
 UpdateView 
 DeleteView
+
