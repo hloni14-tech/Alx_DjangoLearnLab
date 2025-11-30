55c2a3368e7f4cf521ff206c7945ced23d52935c
Neyo <naledihloni14@gmail.com>
2025-11-30 19:05:53 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAuthenticated
+from django_filters import rest_framework 
+from rest_framework import generics
 # Create your views here.
 ListView 
 DetailView 
 CreateView 
 UpdateView 
 DeleteView
+
 
