c7c723a323933a0c6c1293044694189027122859
Neyo <naledihloni14@gmail.com>
2025-12-07 21:20:37 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,4 +6,33 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateField(auto_now_add=True)
-    
+
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+class Profile(models.Model):
+user = models.OneToOneField(User, on_delete=models.CASCADE)
+bio = models.TextField(blank=True)
+image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+
+def __str__(self):
+return f"{self.user.username} Profile"
+
+
+def save(self, *args, **kwargs):
+super().save(*args, **kwargs)
+
+
+# Optionally resize images
+try:
+img = Image.open(self.image.path)
+if img.height > 300 or img.width > 300:
+output_size = (300, 300)
+img.thumbnail(output_size)
+img.save(self.image.path)
+except Exception:
+# when using remote storage or default image, ignore
+pass
