c126fa4f62133c7c28d34e8aafff37d54d0b4991
Neyo <naledihloni14@gmail.com>
2025-12-07 21:21:05 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,30 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-def hello_view(request):
-    return HttpResponse
 
-def book_list(request):
-    books = Book.objects.all()
+# Registration view
+def register(request):
+if request.method == 'POST':
+form = UserRegisterForm(request.POST)
+if form.is_valid():
+user = form.save()
+username = form.cleaned_data.get('username')
+messages.success(request, f'Your account has been created. You can now log in, {username}!')
+return redirect('login')
+else:
+form = UserRegisterForm()
+return render(request, 'blog/register.html', {'form': form})
 
-    context ={'book_list': books}
-    return render(request,'books/book_list.html',context)
 
-from django.views.from django.views.generic import DetailView
-from .models import Book
+# Profile view & edit
+@login_required
+def profile(request):
+if request.method == 'POST':
+u_form = UserUpdateForm(request.POST, instance=request.user)
+p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+if u_form.is_valid() and p_form.is_valid():
+u_form.save()
+p_form.save()
+messages.success(request, 'Your profile has been updated successfully!')
+return redirect('profile')
+else:
+u_form = UserUpdateForm(instance=request.user)
+p_form = ProfileUpdateForm(instance=request.user.profile)
 
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'book/book_detail.html'
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        book = self.get_object()
-        context['average_rating'] = book.get_average_rating()
-
+context = {
+'u_form': u_form,
+'p_form': p_form
+}
+return render(request, 'blog/profile.html', context)
 
 
 # Create your views here.
+
 
