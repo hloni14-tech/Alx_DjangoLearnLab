4b2dd9ce40b8014c9bc60163cc6d0af2328cc340
Neyo <naledihloni14@gmail.com>
2025-12-07 22:14:00 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,2 +1,3 @@
-urlpatterns  [ post/<int:pk>/delete/, post/<int:pk>/update/, post/new/
+urlpatterns  [ post/<int:pk>/delete/, post/<int:pk>/update/, post/new/,
+tags/<slug:tag_slug>/, PostByTagListView.as_view(),
 ]
