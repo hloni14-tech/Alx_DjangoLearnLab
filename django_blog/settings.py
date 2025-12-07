6d7fedd2cc9dc8b35cf07152e1d812dfdd3dd06b
Neyo <naledihloni14@gmail.com>
2025-12-07 19:53:59 +0200

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'blog',
 ]
 
@@ -121,4 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
