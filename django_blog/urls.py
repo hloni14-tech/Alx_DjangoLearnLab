e684ad0c0b99e50497aa03568bfbb4d529ee7408
Neyo <naledihloni14@gmail.com>
2025-12-07 21:55:21 +0200

    Update urls.py

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -26,5 +26,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('comment/posts/<int:post_id>/comments/new/')
+
 ]
+
 
