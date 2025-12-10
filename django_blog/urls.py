30bdc8d2988c78697bc69b0cf7563b1b72a4ae56
Neyo <naledihloni14@gmail.com>
2025-12-10 19:52:13 +0200

    Update urls.py

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -26,7 +26,13 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    path('/posts/<int:post_id>/comments/new/')
+    path('/posts/<int:post_id>/comments/new/'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
 
 ]
 
@@ -69,5 +75,6 @@ urlpatterns = [
     # Delete a post
     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
+
 
 
