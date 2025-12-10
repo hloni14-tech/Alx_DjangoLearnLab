a59c3148724685f4e4dc2008daf91cfcaf418908
Neyo <naledihloni14@gmail.com>
2025-12-10 20:39:13 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,3 +2,5 @@ urlpatterns  [ post/<int:pk>/delete/, post/<int:pk>/update/, post/new/,
 tags/<slug:tag_slug>/, PostByTagListView.as_view(),
 comment/<int:pk>/update/, post/<int:pk>/comments/new/, comment/<int:pk>/delete/
 ]
+
+from django.urls import path "login/", "register/", "profile/"
