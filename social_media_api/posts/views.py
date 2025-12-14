544e8fead569462a276300e2ad331f7015d46c1b
Neyo <naledihloni14@gmail.com>
2025-12-14 20:50:26 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+class PostListView:
+    pass
+
+class PostDetailView:
+    pass
+
+class PostDeleteView:
+    pass
+
+class PostUpdateView:
+    pass
+
+class CommentListView:
+    pass
+
+class CommentDetailView:
+    pass
+
+class CommentDeleteView:
+    pass
+
+class CommentUpdateView:
+    pass
+
+
+
