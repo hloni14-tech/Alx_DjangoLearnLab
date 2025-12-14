c7882c1703646d5d1410dd417ac9dfb2e42692ea
Neyo <naledihloni14@gmail.com>
2025-12-14 20:00:59 +0200

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,1 +1,12 @@
+from rest_framework import serializers
+from .models import Posts, Comments
 
+class PostsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = [ 'author', 'title', 'content', 'created_at', 'updated_at']
+
+class CommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = ['post', 'author', 'content', 'created_at', 'updated_at']
