e4b561a0e29ec03f3f60cd99cd93510935b1390b
Neyo <naledihloni14@gmail.com>
2025-12-14 21:21:43 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -11,3 +11,10 @@ def login_view(request):
 
 def profile_view(request):
 	return JsonResponse({'message': 'profile endpoint'})
+
+def follow_view(request):
+	return JsonResponse({'message': 'follow endpoint'})
+
+def unfollow_view(request):
+	retun JsonResponse({'message': 'unfollow endpoint'})
+
