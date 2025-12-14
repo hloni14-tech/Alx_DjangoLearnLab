105a3538fb9dff1d77060dc8d0b8f9d9922251e0
Neyo <naledihloni14@gmail.com>
2025-12-14 22:22:12 +0200

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('profile/', views.profile_view, name='profile'),
+    path('follow/<int:user_id>'),
+    path('unfollow/<int:user_id>/'),
 ]
+
