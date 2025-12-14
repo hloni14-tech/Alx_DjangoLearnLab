8644c02d9755f4e44f1bc0b8d37501fffdffcf18
Neyo <naledihloni14@gmail.com>
2025-12-14 22:38:36 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -71,7 +71,14 @@ class UnlikePostView(GnericAPIView):
         post_id = kwargs.get('post_id')
         return Response({"message": f"Post {post_id} unliked."})
 
+class NotificationListView(GnericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return Response({"message": "List of notifications."})
+
  
+
 
 
 
