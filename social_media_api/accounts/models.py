17e2c216debe0fe4829046ac0508e00fddadbf14
Neyo <naledihloni14@gmail.com>
2025-12-14 19:21:14 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Account(models.Model):
     bio = models.TextField()
     profile_picture = models.ImageField(upload_to='profile_pics/')
     followers = models.ManyToManyField('self', symmetrical=False)
+
     
