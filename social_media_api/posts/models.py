6f3f623a2fcf63a5e9046cf6a7052f94685a53c2
Neyo <naledihloni14@gmail.com>
2025-12-14 19:54:39 +0200

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    post = models.ForeignKey(Posts, related_name='comments', on_delete=models.C
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+
