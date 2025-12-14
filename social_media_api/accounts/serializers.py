a78106bc2b3cce1c0e36137ae005f5af98d529f0
Neyo <naledihloni14@gmail.com>
2025-12-14 19:38:30 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,1 +1,15 @@
+from rest_framework import serializers
+from .models import SocialMediaAccount
+from django.contrib.auth.models import User
+
+class SocialMediaAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialMediaAccount
+        fields = ['bio', 'profille_picture', 'followers']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
 
